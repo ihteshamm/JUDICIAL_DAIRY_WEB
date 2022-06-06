57e0c7344d99b6a550401c63e22bd3c618e2d9a2
Ihtesham Mansoor <ihteshamm112@gmail.com>
2022-06-06 11:44:35 +0500

    delete

--- client/src/components/layout/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-dark text-white mt-5 pd-4 text-center">
-       Copyright © 2018-2022 COMSATS. All Rights Reserved.
-    </footer>
-  );
-}
