cbe90e687967cdb92e85eb920c4cd96f77ca38e9
Ihtesham Mansoor <ihteshamm112@gmail.com>
2022-07-18 12:00:51 +0500

    HerokuUpdate

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,6 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
-      secretOrKey: 'YOUR_SECRET'
+//    mongoURI: 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false',
+    mongoURI: 'mongodb+srv://ihtesham:khattak212@cluster0.x2yyd.mongodb.net/?retryWrites=true&w=majority',
+    secretOrKey: 'YOUR_SECRET'
 };
 SKIP_PREFLIGHT_CHECK=true
